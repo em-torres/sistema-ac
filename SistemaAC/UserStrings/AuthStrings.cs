75ca0a8e8cc2a5b10bcf7ce3498d1134e4c7156c
Edwin Marte Torres <kdgpro15@gmail.com>
2019-05-23 11:58:14 -0400

    Vid_20 - Modifying Access Denied View

---
+++ SistemaAC/UserStrings/AuthStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaAC.UserStrings
+{
+    public class AuthStrings
+    {
+        public const string AcDenDesc = "Usted no tiene acceso a este recurso.";
+        public const string AcDenTitle = "Acceso Denegado";
+    }
+}
